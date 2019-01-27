4910ce8b9101c01cc137345959ae1cc18fcb2bad
Dekion <dekion@protonmail.com>
2019-01-27 19:08:19 +0300

    bug fix

--- engine/ui/utils.py
+++ engine/ui/utils.py
@@ -9,7 +9,7 @@ class Drawer:
         # Draw ┌───┐
         self.display.draw((x, y), extra[2] + extra[0]*(w-2) + extra[3])
         # Draw │   │
-        for i in range(y+1, h):
+        for i in range(y+1, y+h):
             self.display.draw((x, i), extra[1] + ' '*(w-2) + extra[1])
         # Draw └───┘
-        self.display.draw((x, h), extra[4] + extra[0]*(w-2) + extra[5])
+        self.display.draw((x, y+h), extra[4] + extra[0]*(w-2) + extra[5])
